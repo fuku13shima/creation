267351fb145e5144df0a701d5c6e89616e99b017
fukushima <22110051@oic-ok.ac.jp>
2023-07-07 00:14:03 +0900

    FizzBuzz修正

--- FizzBuzz/js/app.js
+++ FizzBuzz/js/app.js
@@ -1,9 +1,9 @@
 let cnt = 0;
 let fizzFlg = false;
 let buzzFlg = false;
-let fontSizeOrigen = $('#num').css("font-size");
-// let fontWeightOrigen = $('#num').css("width");
-// console.log('size' + fontSizeOrigen + ' : weight' + fontWeightOrigen);
+// let originSize = $('#num').css("font-size");
+// let fontWeightOrigen = $('#num').css("font-weight");
+// console.log(originSize);
 //let num = document.getElementById('num');
 //console.log(num);
 
@@ -11,34 +11,54 @@ let fontSizeOrigen = $('#num').css("font-size");
 $(document).ready(function() {
     cnt = 0;
     $('#num').text(cnt);
-
   });
+
+$(function(){
+    min: 0
+    $('#num').knob({
+        'release' : function (v) {
+            // console.log(v);
+            cnt = v;
+            FizzBuzzCheck(v);
+        }
+    });
+    
+});
 
 //UPボタン押したときカウントアップ
 countUpButton.addEventListener('click' , () => {
     cnt++;
-    $('#num').text(cnt);
+    $('#num').val(cnt);
     // console.log('countUp' + cnt);
+    // console.log($('#num').val());
 
+    $('#num').knob({});
     FizzBuzzCheck(cnt);
 
 });
 
 //DOWNボタン押したときカウントダウン
 countDownButton.addEventListener('click' , () => {
+    if(cnt > 0){
     cnt--;
-    $('#num').text(cnt);
+    $('#num').val(cnt);
     // console.log('countDown' + cnt);
 
+    $('#num').knob({});
     FizzBuzzCheck(cnt);
+
+    }else{
+        alert('0以下は入力できません');
+    }
 
 });
 
 //RESETボタン押したときカウントリセット
 resetButton.addEventListener('click' , () => {
+    $('#num').val('0')
     cnt = 0;
     $('#checkResult').text('');
-    $('#num').text(cnt);
+    // $('#num').text(cnt);
     // console.log('reset' + cnt);
 
     FizzBuzzCheck(cnt);
@@ -49,9 +69,8 @@ resetButton.addEventListener('click' , () => {
 function FizzBuzzCheck(cnt){
     $('#checkResult').css('display' , 'none');
     $('#checkResult').text('');
-    $('#num').css('color' , 'black');
-    $('#num').css('font-size' , fontSizeOrigen);
-    // $('#num').css('width' , fontWeightOrigen);
+    $('#num').css('color' , '#87CEEB');
+    $('#num').css('font-size' , '200%');
     // console.log('FizzBuzzCheck' + cnt);
 
     if(cnt != 0){
@@ -69,15 +88,17 @@ function FizzBuzzCheck(cnt){
 
     if(fizzFlg && buzzFlg){
         $('#num').css('color' , 'orangered');
-        $('#num').css('font-size' , '+=20');
-        // $('#num').css('width' , '40px');
+        $('#num').css('font-size' , '+=30');
+        $('#num').css('font-weight' , '1000');
         // console.log('size' + $('#num').css("font-size") + ' : weight' + $('#num').css("font-weight"));
+
 
     }else if (fizzFlg || buzzFlg){
         $('#num').css('color' , 'coral');
-        $('#num').css('font-size' , '+=10');
-        // $('#num').css('width' , '20px');
+        $('#num').css('font-size' , '+=20');
+        $('#num').css('font-weight' , '900');
         // console.log('size' + $('#num').css("font-size") + ' : weight' + $('#num').css("font-weight"));
+
 
     }
 
